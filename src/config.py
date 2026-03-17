0a327d6be730fdf8c316ff6d93547ae807d1c8ce
MrJoeKr <kraus.jozef225@gmail.com>
2026-03-17 21:48:16 +0100

    autoresearch: reduce image size to 192 (26% fewer pixels)

--- src/config.py
+++ src/config.py
@@ -34,7 +34,7 @@ class Config:
         "Tomato_healthy",
     ])
     unknown_class: str = "Tomato_Bacterial_spot"
-    img_size: int = 224
+    img_size: int = 192
     num_workers: int = 4
 
     # Model
