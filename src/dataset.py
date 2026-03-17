01e3c9a84aebfee551bc24dcb44ea7298bfffec7
MrJoeKr <kraus.jozef225@gmail.com>
2026-03-17 21:56:29 +0100

    autoresearch: lighter augmentation (remove ColorJitter, RandomRotation)

--- src/dataset.py
+++ src/dataset.py
@@ -71,8 +71,6 @@ def get_train_transform(img_size: int = 224):
         transforms.RandomResizedCrop(img_size, scale=(0.8, 1.0)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomVerticalFlip(),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
-        transforms.RandomRotation(15),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
